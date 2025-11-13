97d17737f81002b1eea01dc4d70596838173902b
henryccy <104284652+henryccy@users.noreply.github.com>
2025-11-13 13:44:56 +0800

    Update params.cc

--- common/params.cc
+++ common/params.cc
@@ -578,7 +578,6 @@ std::unordered_map<std::string, uint32_t> keys = {
     {"WheelIcon", PERSISTENT},
     {"WheelSpeed", PERSISTENT},
     {"WheelToDownload", CLEAR_ON_MANAGER_START},
-    {"MapdSettings", {PERSISTENT, JSON}},
 };
 
 } // namespace
