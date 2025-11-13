f43a9eda50050f00d055c4f06082ac7a1e51e39c
henryccy <104284652+henryccy@users.noreply.github.com>
2025-11-13 13:26:46 +0800

    Revert "Update params.cc"
    This reverts commit 74d1fe1d72e5c67c639fa0ca9180d4eb299faa36.

--- common/params.cc
+++ common/params.cc
@@ -578,7 +578,7 @@ std::unordered_map<std::string, uint32_t> keys = {
     {"WheelIcon", PERSISTENT},
     {"WheelSpeed", PERSISTENT},
     {"WheelToDownload", CLEAR_ON_MANAGER_START},
-    {"MapdSettings", PERSISTENT},
+    {"MapdSettings", {PERSISTENT, JSON}},
 };
 
 } // namespace
