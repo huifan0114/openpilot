67857816adadedf658fb5d9e3bca409f6031d7ec
huifan <huifan0114@gmail.com>
2025-12-11 22:17:01 +0800

    Update text.cc

--- selfdrive/ui/qt/text.cc
+++ selfdrive/ui/qt/text.cc
@@ -47,14 +47,14 @@ int main(int argc, char *argv[]) {
 
   QPushButton *btn = new QPushButton();
 #ifdef __aarch64__
-  btn->setText(QObject::tr("Restore"));
+  btn->setText(QObject::tr("重新安裝並重啟"));
   QObject::connect(btn, &QPushButton::clicked, [=]() {
     btn->setEnabled(false);
 
     runRestore();
   });
 #else
-  btn->setText(QObject::tr("Exit"));
+  btn->setText(QObject::tr("離開"));
   QObject::connect(btn, &QPushButton::clicked, &a, &QApplication::quit);
 #endif
   main_layout->addWidget(btn, 0, 0, Qt::AlignRight | Qt::AlignBottom);
