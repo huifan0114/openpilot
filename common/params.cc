5a33619f70eda8f2c37100c9e5e2afb22cc6169d
huifan0114 <huifan0114@gmail.com>
2025-12-16 11:26:25 +0800

    Update params.cc

--- common/params.cc
+++ common/params.cc
@@ -217,7 +217,7 @@ std::unordered_map<std::string, uint32_t> keys = {
 
 /////////////////////////////////////////////
     {"HFOPinf", PERSISTENT},
-    {"GooffScreen", PERSISTENT | FROGPILOT_STORAGE | FROGPILOT_VISUALS},
+    {"GooffScreen", PERSISTENT},
     // {"Faststart", PERSISTENT},
     {"Fuelprice", PERSISTENT},
     {"Fuelcosts", PERSISTENT},
