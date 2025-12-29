fc022f57e06bd74f0a8b5beeca4fff6e227113be
huifan <huifan0114@gmail.com>
2025-12-29 23:44:40 +0800

    Refactor HFOP settings and update drive summary layout
    Reorganized the HFOP settings panel logic, grouping related toggles and updating their visibility conditions. Adjusted the order and structure of panel buttons in frogpilot_settings.cc. In drive_summary.cc, refactored the display of fuel consumption and fuel cost to appear side by side for improved UI clarity.

--- frogpilot/ui/qt/offroad/hfop_settings.cc
+++ frogpilot/ui/qt/offroad/hfop_settings.cc
@@ -43,41 +43,42 @@ FrogPilotHFOPPanel::FrogPilotHFOPPanel(FrogPilotSettingsWindow *parent) : FrogPi
 
 
   const std::vector<std::tuple<QString, QString, QString, QString>> hfopToggles {
-    {"HFOPinf", "  訊息框", "主畫面左下方顯示訊息狀態.", "../assets/offroad/icon_custom.png"},
-    {"AutoACC", "自動啟動ACC", "啟用後會自動啟動ACC.", "../assets/offroad/icon_conditional.png"},
+    {"HFOPinf", "  訊息框", "主畫面左下方顯示訊息狀態.", ""},
+    {"AutoACC", "  自動啟動ACC", "啟用後會自動啟動ACC.", ""},
     {"AutoACCspeed", "  自動啟動ACC時速設定", "設定自動啟動ACC的時速條件設定.", ""},
     {"AutoACCCarAway", "  前車遠離啟動", "啟用後當前方車輛遠離會自動啟動ACC.", ""},
     {"AutoACCGreenLight", "  綠燈啟動", "啟用後當偵測到綠燈時會自動啟動ACC.", ""},
 
-    {"Roadtype", "道路種類設定", "開啟後可依道路種類在特定條件下預設時速", "../assets/offroad/icon_road.png"},
-    {"AutoRoadtype", "自動道路種類設定", "開啟後可自動依道路種類在特定條件下預設時速", ""},
-    {"RoadtypeProfile", "道路種類設定", "開啟後可依道路種類在特定條件下預設時速", ""},
+    {"Roadtype", "  道路種類設定", "開啟後可依道路種類在特定條件下預設時速", ""},
+    {"AutoRoadtype", "  自動道路種類設定", "開啟後可自動依道路種類在特定條件下預設時速", ""},
+    {"RoadtypeProfile", "  道路種類設定", "開啟後可依道路種類在特定條件下預設時速", ""},
 
-    {"ChangeLaneReminder", "  變換車道語音", "開啟後在變換車道時會發出語音提醒.", "../assets/offroad/icon_warning.png"},
-    {"AutoSpeeddistance", "車速調控跟車距離", "開啟後可依行車路線自動切換跟車距離， 1格 60公里 2格90公里 3格120公里.", "../assets/offroad/icon_distance.png"},
+    {"TrafficMode", "  塞車模式", "按住「距離」按鈕 2.5 秒，可根據走走停停的交通狀況啟用更激進的駕駛行為.", ""},
+    {"TrafficModespeed", "  塞車模式時速設定", "低於此速度將自動啟動塞車模式.", ""},
 
-    {"Navspeed", "圖資速限", "開啟後可依當下所在道路的圖資速限自動更新.", "../assets/offroad/icon_map.png"},
+    {"VagSpeed", "  時速差調整", "VAG專用。調整車錶速度與C3定速設定不同步的問題。", ""},
+    {"VagSpeedFactor", "  時速差調整", "請輸入OP定速為110時儀表板的速度差值.", ""},
+    {"Disablestartstop", "取消怠速熄火", "開啟後將強制關閉怠速熄火功能.", ""},
+
+    {"ChangeLaneReminder", "  變換車道語音", "開啟後在變換車道時會發出語音提醒.", ""},
+    {"AutoSpeeddistance", "  車速調控跟車距離", "開啟後可依行車路線自動切換跟車距離， 1格 60公里 2格90公里 3格120公里.", ""},
+
+    {"Navspeed", "  圖資速限", "開啟後可依當下所在道路的圖資速限自動更新.", ""},
     {"NavReminder", "  導航語音", "開啟後若使用道路導航時會播報轉彎語音訊息.", ""},
     {"speedoverreminder", "  超速提醒", "開啟後若當下速度高於圖資速限會發出提醒.", ""},
     {"speedreminderreset", "  超速重設速限", "開啟後若當下速度高於圖資速限會強制重設速限.", ""},
 
-    {"Dooropen", "  車門開啟", "開啟後在引擎啟動狀態下駕駛車門開啟或後車箱未關閉時會發出提醒.", "../assets/offroad/icon_warning.png"},
+    {"Dooropen", "  車門開啟", "開啟後在引擎啟動狀態下駕駛車門開啟或後車箱未關閉時會發出提醒.", ""},
     {"DriverdoorOpen", "  駕駛車門", "開啟後在引擎啟動狀態下駕駛車門開啟時會發出提醒.", ""},
     {"CodriverdoorOpen", "  副駕駛車門", "開啟後在引擎啟動狀態下副駕駛車門開啟時會發出提醒.", ""},
     {"LpassengerdoorOpen", "  左乘客車門", "開啟後在引擎啟動狀態下左乘客車門開啟時會發出提醒.", ""},
     {"RpassengerdoorOpen", "  右乘客車門", "開啟後在引擎啟動狀態下右乘客車門開啟時會發出提醒.", ""},
     {"LuggagedoorOpen", "  後車門開啟", "開啟後在引擎啟動狀態下候車門開啟時會發出提醒.", ""},
 
-    {"TrafficMode", tr("  塞車模式"), tr("按住「距離」按鈕 2.5 秒，可根據走走停停的交通狀況啟用更激進的駕駛行為."), ""},
-    {"TrafficModespeed", "  塞車模式時速設定", "低於此速度將自動啟動塞車模式.", ""},
-
-    {"Fuelprice", "油價計算", "啟動後會計算油費.", "../frogpilot/assets/toggle_icons/icon_light.png"},
+    {"Fuelprice", "  油價計算", "啟動後會計算油費.", ""},
     {"Fuelcosts", "油價設定", "設定車輛使用油種與價格.", ""},
 
-    {"VagSpeed", "時速差調整", "VAG專用。調整車錶速度與C3定速設定不同步的問題。", "../assets/offroad/icon_openpilot.png"},
-    {"VagSpeedFactor", "  時速差調整", "請輸入OP定速為110時儀表板的速度差值.", ""},
 
-    {"Disablestartstop", "取消怠速熄火", "開啟後將強制關閉怠速熄火功能.", "../assets/offroad/icon_warning.png"},
   };
 
   for (const auto &[param, title, desc, icon] : hfopToggles) {
@@ -93,16 +94,17 @@ FrogPilotHFOPPanel::FrogPilotHFOPPanel(FrogPilotSettingsWindow *parent) : FrogPi
     } else if (param == "AutoACCspeed") {
       hfopcontrolsToggle = new FrogPilotParamValueControl(param, title, desc, icon, 1, 50, "公里");
 
-    } else if (param == "Fuelprice") {
-      FrogPilotManageControl *FuelpriceToggle = new FrogPilotManageControl(param, title, desc, icon);
-      QObject::connect(FuelpriceToggle, &FrogPilotManageControl::manageButtonClicked, [hfopLayout, FuelpricePanel]() {
-        hfopLayout->setCurrentWidget(FuelpricePanel);
+    } else if (param == "Roadtype") {
+      FrogPilotManageControl *RoadToggle = new FrogPilotManageControl(param, title, desc, icon);
+      QObject::connect(RoadToggle, &FrogPilotManageControl::manageButtonClicked, [hfopLayout, RoadtypePanel]() {
+        hfopLayout->setCurrentWidget(RoadtypePanel);
       });
-      hfopcontrolsToggle = FuelpriceToggle;
+      hfopcontrolsToggle = RoadToggle;
 
-    } else if (param == "Fuelcosts") {
-      hfopcontrolsToggle = new FrogPilotParamValueControl(param, title, desc, icon, 30.0, 36.0, "元", std::map<float, QString>(),0.1);
-
+    } else if (param == "RoadtypeProfile") {
+      std::vector<QString> profileOptions{tr("關閉"), tr("巷弄"),tr("平面"), tr("快速"), tr("高速")};
+      ButtonParamControl *profileSelection = new ButtonParamControl(param, title, desc, icon, profileOptions);
+      hfopcontrolsToggle = profileSelection;
 
     } else if (param == "TrafficMode") {
       FrogPilotManageControl *TrafficModeToggle = new FrogPilotManageControl(param, title, desc, icon);
@@ -114,38 +116,35 @@ FrogPilotHFOPPanel::FrogPilotHFOPPanel(FrogPilotSettingsWindow *parent) : FrogPi
     } else if (param == "TrafficModespeed") {
       hfopcontrolsToggle = new FrogPilotParamValueControl(param, title, desc, icon, 0, 60, "公里");
 
-
     } else if (param == "VagSpeed") {
       FrogPilotManageControl *VagSpeedToggle = new FrogPilotManageControl(param, title, desc, icon);
       QObject::connect(VagSpeedToggle, &FrogPilotManageControl::manageButtonClicked, [hfopLayout, VagSpeedPanel]() {
         hfopLayout->setCurrentWidget(VagSpeedPanel);
       });
       hfopcontrolsToggle = VagSpeedToggle;
+
     } else if (param == "VagSpeedFactor") {
       hfopcontrolsToggle = new FrogPilotParamValueControl(param, title, desc, icon, 0, 20, "公里");
-
-
-
-    } else if (param == "Roadtype") {
-      FrogPilotManageControl *RoadToggle = new FrogPilotManageControl(param, title, desc, icon);
-      QObject::connect(RoadToggle, &FrogPilotManageControl::manageButtonClicked, [hfopLayout, RoadtypePanel]() {
-        hfopLayout->setCurrentWidget(RoadtypePanel);
-      });
-      hfopcontrolsToggle = RoadToggle;
-    } else if (param == "RoadtypeProfile") {
-      std::vector<QString> profileOptions{tr("關閉"), tr("巷弄"),tr("平面"), tr("快速"), tr("高速")};
-      ButtonParamControl *profileSelection = new ButtonParamControl(param, title, desc, icon, profileOptions);
-      hfopcontrolsToggle = profileSelection;
 
     } else if (param == "Navspeed") {
       hfopcontrolsToggle = new ParamControl(param, title, desc, icon);
 
-    } else if(param == "Dooropen") {
+    } else if (param == "Dooropen") {
       FrogPilotManageControl *DooropenToggle = new FrogPilotManageControl(param, title, desc, icon);
       QObject::connect(DooropenToggle, &FrogPilotManageControl::manageButtonClicked, [hfopLayout, DooropenPanel]() {
         hfopLayout->setCurrentWidget(DooropenPanel);
       });
       hfopcontrolsToggle = DooropenToggle;
+
+    } else if (param == "Fuelprice") {
+      FrogPilotManageControl *FuelpriceToggle = new FrogPilotManageControl(param, title, desc, icon);
+      QObject::connect(FuelpriceToggle, &FrogPilotManageControl::manageButtonClicked, [hfopLayout, FuelpricePanel]() {
+        hfopLayout->setCurrentWidget(FuelpricePanel);
+      });
+      hfopcontrolsToggle = FuelpriceToggle;
+
+    } else if (param == "Fuelcosts") {
+      hfopcontrolsToggle = new FrogPilotParamValueControl(param, title, desc, icon, 30.0, 36.0, "元", std::map<float, QString>(),0.1);
 
     } else {
       hfopcontrolsToggle = new ParamControl(param, title, desc, icon);
@@ -199,7 +198,7 @@ FrogPilotHFOPPanel::FrogPilotHFOPPanel(FrogPilotSettingsWindow *parent) : FrogPi
     });
   }
 
-  QSet<QString> forceUpdateKeys = {"AutoACC"};
+  QSet<QString> forceUpdateKeys = {"AutoACC", "Roadtype", "TrafficMode", "VagSpeed", "Navspeed", "Dooropen", "Fuelprice"};
   for (const QString &key : forceUpdateKeys) {
     QObject::connect(static_cast<ToggleControl*>(toggles[key]), &ToggleControl::toggleFlipped, this, &FrogPilotHFOPPanel::updateToggles);
   }
@@ -236,7 +235,7 @@ void FrogPilotHFOPPanel::updateToggles() {
 
     bool setVisible = parent->tuningLevel >= frogpilotToggleLevels[key].toDouble();
 
-    if (key == "Fuelprice") {
+    if (key == "Roadtype") {
       setVisible &= parent->hasOpenpilotLongitudinal;
     }
 
@@ -248,15 +247,15 @@ void FrogPilotHFOPPanel::updateToggles() {
       setVisible &= parent->hasOpenpilotLongitudinal;
     }
 
-    else if (key == "Roadtype") {
-      setVisible &= parent->hasOpenpilotLongitudinal;
-    }
-
     else if (key == "Navspeed") {
       setVisible &= parent->hasOpenpilotLongitudinal;
     }
 
     else if (key == "Dooropen") {
+      setVisible &= parent->hasOpenpilotLongitudinal;
+    }
+
+    else if (key == "Fuelprice") {
       setVisible &= parent->hasOpenpilotLongitudinal;
     }
 
@@ -269,26 +268,30 @@ void FrogPilotHFOPPanel::updateToggles() {
         toggles["AutoACCCarAway"]->setVisible(autoACCEnabled);
         toggles["AutoACCGreenLight"]->setVisible(autoACCEnabled);
 
-      } else if (key == "Fuelprice") {
-        toggles["Fuelcosts"]->setVisible(true);
-      } else if (key == "TrafficMode") {
-        toggles["TrafficModespeed"]->setVisible(true);
-      } else if (key == "VagSpeed") {
-        toggles["VagSpeedFactor"]->setVisible(true);
-
       } else if (key == "Roadtype") {
         toggles["AutoRoadtype"]->setVisible(true);
         toggles["RoadtypeProfile"]->setVisible(true);
+
+      } else if (key == "TrafficMode") {
+        toggles["TrafficModespeed"]->setVisible(true);
+
+      } else if (key == "VagSpeed") {
+        toggles["VagSpeedFactor"]->setVisible(true);
+
       } else if (key == "Navspeed") {
         toggles["NavReminder"]->setVisible(true);
         toggles["speedoverreminder"]->setVisible(true);
         toggles["speedreminderreset"]->setVisible(true);
+
       } else if (key == "Dooropen") {
         toggles["DriverdoorOpen"]->setVisible(true);
         toggles["CodriverdoorOpen"]->setVisible(true);
         toggles["LpassengerdoorOpen"]->setVisible(true);
         toggles["RpassengerdoorOpen"]->setVisible(true);
         toggles["LuggagedoorOpen"]->setVisible(true);
+
+      } else if (key == "Fuelprice") {
+        toggles["Fuelcosts"]->setVisible(true);
       }
     }
   }
