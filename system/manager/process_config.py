dcd70d8e9bb236f52866ce6911e7283086d70e43
huifan0114 <108561744+huifan0114@users.noreply.github.com>
2026-01-21 10:07:06 +0800

    Update process_config.py

--- system/manager/process_config.py
+++ system/manager/process_config.py
@@ -107,7 +107,7 @@ procs = [
 ############################################################################################
   PythonProcess("qcomgpsd", "system.qcomgpsd.qcomgpsd", qcomgps, enabled=TICI),
   #PythonProcess("ugpsd", "system.ugpsd", only_onroad, enabled=TICI),
-  #PythonProcess("navd", "selfdrive.navd.navd", only_onroad),  # 停用，使用 navbridged 取代
+  PythonProcess("navd", "selfdrive.navd.navd", only_onroad),  # 停用，使用 navbridged 取代
   PythonProcess("pandad", "selfdrive.pandad.pandad", always_run),
   PythonProcess("paramsd", "selfdrive.locationd.paramsd", only_onroad),
   PythonProcess("lagd", "selfdrive.locationd.lagd", only_onroad),
@@ -128,14 +128,15 @@ procs = [
   NativeProcess("bridge", "cereal/messaging", ["./bridge"], notcar),
   PythonProcess("webrtcd", "system.webrtc.webrtcd", notcar),
   PythonProcess("webjoystick", "tools.bodyteleop.web", notcar),
-  PythonProcess("simple_dash", "tools.simple_dash.server", run_simple_dash),
+  #PythonProcess("simple_dash", "tools.simple_dash.server", run_simple_dash),
 
   # FrogPilot processes
   NativeProcess("classic_modeld", "frogpilot/classic_modeld", ["./classic_modeld"], run_classic_modeld),
   PythonProcess("frogpilot_process", "frogpilot.frogpilot_process", always_run),
   PythonProcess("mapd", "frogpilot.navigation.mapd", always_run),
+  #PythonProcess("navbridged", "selfdrive.navd.navbridged", run_navbridge),
   PythonProcess("speed_limit_filler", "frogpilot.system.speed_limit_filler", run_speed_limit_filler),
-  #PythonProcess("the_pond", "frogpilot.system.the_pond.the_pond", always_run),
+  PythonProcess("the_pond", "frogpilot.system.the_pond.the_pond", always_run),
   PythonProcess("tinygrad_modeld", "frogpilot.tinygrad_modeld.tinygrad_modeld", run_tinygrad_modeld),
 ]
 
